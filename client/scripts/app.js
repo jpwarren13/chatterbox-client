ab4f8bb6543da2836aee0e8e8ede3b208ef8e8a9
seeThisThrough <geedyne.li@gmail.com>
2018-09-28 14:58:32 -0500

    set up skeleton

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,45 @@
 // YOUR CODE HERE:
+
+class App  {
+  constructor(){
+
+  }
+
+
+  init(){
+  
+
+  };
+  
+  //should submit POST request via $.ajax
+  send(){
+
+
+  };
+
+
+
+
+}
+
+let app = new App()
+// var message = {
+//     username: 'shawndrost',
+//     text: 'trololo',
+//     roomname: '4chan'
+//   };
+
+//  $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   }
+// });
