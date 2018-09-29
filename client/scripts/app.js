39324eccbc24f2e6887e2e7c1846f30576c91b5f
johnpwarren@gmail.com <johnpwarren@gmail.com>
2018-09-29 09:40:36 -0700

    sort of there

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,35 +3,35 @@
 class App  {
     constructor(){
      this.server = 'http://parse.atx.hackreactor.com/chatterbox/classes/messages';
+    // this.renderRoom('lobby');
     }
     
     
     init(){
-        
+      $(function(){
+        this.renderRoom('lobby');
+      })
+
+
         
     };
     
     //should submit POST request via $.ajax
     send(messageToSend){
         $.ajax({
-            // This is the url you should use to communicate with the parse API server.
-            url: this.server,
-            type: 'POST',
-            data: JSON.stringify(messageToSend), //object with correct format
+            type: "POST",
+            url:  'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+            data: JSON.stringify(messageToSend),
             contentType: 'application/json',
-            success: function (data) {
-            console.log('chatterbox: Message sent');
-            },
-            error: function (data) {
-            // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-            console.error('chatterbox: Failed to send message', data);
+            success: function(resultData){
+                alert("Save Complete");
             }
-        });
+      });
 
 
   };
   // submits a GET request using $.ajax
-  fetch(n){
+  fetch(){
  
     let f = $.ajax({
         // This is the url you should use to communicate with the parse API server.
@@ -57,18 +57,22 @@ $('#chats').empty();
   };
   
   // adds message to the DOM
-  renderMessage(message){
+  renderMessage(text, user){
 //et data = this.fetch();
+let message = '<div class ="chat"' + text + '</div>';
 
- $('#chats').append('<div class="chat">'+JSON.stringify(message.text)+ '</div>');
 
-      //get message to render
-      //
-
+$('#chats').append(message);
   };
   // adds rooms to the DOM
-  renderRoom(){
+  renderRoom(roomName){
 
+$('#roomSelect').append('<option>' + roomName +  '</option>')
+// for (let i = 0; i < messagesArray.length; i++){
+//     let newChatDiv = '<div class="chat"' + messagesArray[i].text + '</div>';
+//     let newUserDiv = '<div class="username"' + messagesArray[i].username + '</div>';
+//     $('#chats').append(newChatDiv).append(newUserDiv);
+// }
 
 
   };
@@ -81,13 +85,17 @@ $('#chats').empty();
 
   //sends message on click
   handleSubmit(){
-
+    $('.submit-button').on('click', function(){
+        var messageText = $('.message-input').val();
+        this.renderMessage(messageText);
+    })
 
 
   };
 }
 
 let app = new App()
+app.init();
 // var message = {
 //     username: 'shawndrost',
 //     text: 'trololo',
